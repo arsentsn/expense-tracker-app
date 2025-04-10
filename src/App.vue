c341bc6a6391125cd2acd89890b93a35bcf3b4eb
atsntsgo <tsnarsen@gmail.com>
2025-04-10 18:15:29 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <h1>Expense Tracker</h1>
+    <!-- Your components will go here -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+    // Register your components here
+  },
+}
+</script>
+
+<style>
+/* Your base styles */
+</style>
